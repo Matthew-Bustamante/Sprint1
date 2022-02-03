e14cacb4a80e742b7bcaece13f18988ec957234d
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-02-03 09:01:26 -0700

    A working skeleton version of the print method

--- Crud.java
+++ Crud.java
@@ -8,6 +8,7 @@
 import java.io.*;
 import java.util.Scanner;
 
+
 public class Crud {
 
     public static void main(String[] args) {
@@ -29,5 +30,17 @@ public class Crud {
     public void delete() {
 
     }
-
+    
+    public void printData() {
+    	int[][] data = new int [4][4]; //a set 2D array but can be changed and data type can be changed as well
+    	
+    	
+    	for(int row = 0; row < data.length; row++ ) { //for loop to loop the first part of the array
+    		for(int collumn = 0; collumn < data[row].length; collumn++) {
+    			data[row][collumn] = row * collumn;
+    			System.out.println(data[row][collumn] + "\t");// will print the data
+    		}
+    		System.out.println();// will print a new line
+    	}
+    }
 }
