0ec0a8055e031ec4dd081125638c324f819eb72a
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-02-04 17:48:16 -0700

    Main method now asks for file input & prints the action menu correctly

--- Crud.java
+++ Crud.java
@@ -14,26 +14,29 @@ public class Crud {
     public static void main(String[] args) {
     	boolean notDone = true;
     	Crud project = new Crud();
+    	Scanner fileName = new Scanner(System.in);
+    	System.out.println("Enter a File Location");
+    	String fileNm = fileName.nextLine();
     	while(notDone) {
-    		System.out.println("Enter Q to quit, C to create, R to read, U to update, D to delete");
-    		System.out.println("or P to print");
+    		System.out.println("Enter a Number to Preform an action");
+    		System.out.println("1.) Create \n2.) Read \n3.) Update \n4.) Delete \n5.) Exit");
     		Scanner userInput = new Scanner(System.in);
     		System.out.println("Enter a valid input");
     		String input = userInput.nextLine();
     		
-    		if(input.equals("Q")) {
+    		if(input.equals("5")) {
     			notDone = false;
     		}
-    		else if(input.equals("C")) {
+    		else if(input.equals("1")) {
     			project.create();
     		}
-    		else if(input.equals("R")) {
+    		else if(input.equals("2")) {
     			project.read();
     		}
-    		else if(input.equals("U")) {
+    		else if(input.equals("3")) {
     			project.update();
     		}
-    		else if(input.equals("D")) {
+    		else if(input.equals("4")) {
     			project.delete();
     		}
     		else if(input.equals("P")) {
